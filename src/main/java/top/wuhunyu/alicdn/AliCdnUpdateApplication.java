d0e902aeedd89e564bb00757ea4ff16af916b64c
gongzhiqiang <wuhunyu@gmail.com>
2024-06-24 18:35:31 +0800

    feat(*): 优化日志大小写问题

--- src/main/java/top/wuhunyu/alicdn/AliCdnUpdateApplication.java
+++ src/main/java/top/wuhunyu/alicdn/AliCdnUpdateApplication.java
@@ -22,7 +22,7 @@ public class AliCdnUpdateApplication {
     private static void schedule() {
         AliCdnProperties instance = AliCdnProperties.getInstance();
         CronUtil.schedule(instance.getScheduledCron(), (Runnable) () -> {
-            log.info("触发定时器，执行阿里云 cdn 证书更新任务");
+            log.info("触发定时器，执行阿里云 CDN 证书更新任务");
             SetCdnDomainSSLCertificate.invoke();
         });
         // 设置支持秒级任务
@@ -40,7 +40,7 @@ public class AliCdnUpdateApplication {
 
     public static void main(String[] args) throws InterruptedException {
         // 启动应用
-        log.info("阿里云cdn更新程序启动中");
+        log.info("阿里云 CDN 更新程序启动中");
         // 启动定时任务
         schedule();
         // 启动证书监听器
