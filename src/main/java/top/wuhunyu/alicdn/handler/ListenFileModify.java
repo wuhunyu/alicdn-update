8fedb51741eb8f5c9a26e3e146c982eb7a74ef49
gongzhiqiang <wuhunyu@gmail.com>
2024-06-24 09:17:36 +0800

    fix(监听文件修改事件): 修复公私钥发生变化时，监听不生效问题

--- src/main/java/top/wuhunyu/alicdn/handler/ListenFileModify.java
+++ src/main/java/top/wuhunyu/alicdn/handler/ListenFileModify.java
@@ -3,6 +3,7 @@ package top.wuhunyu.alicdn.handler;
 import cn.hutool.core.io.watch.SimpleWatcher;
 import cn.hutool.core.io.watch.WatchMonitor;
 import cn.hutool.core.io.watch.watchers.DelayWatcher;
+import lombok.extern.slf4j.Slf4j;
 import top.wuhunyu.alicdn.properties.AliCdnProperties;
 
 import java.nio.file.Path;
@@ -16,6 +17,7 @@ import java.util.Objects;
  * @date 2024/06/23 16:07
  **/
 
+@Slf4j
 public class ListenFileModify {
 
     public static void listen(final Runnable work) {
@@ -23,16 +25,18 @@ public class ListenFileModify {
         WatchMonitor.createAll(aliCdnProperties.getSslPath(), new DelayWatcher(new SimpleWatcher() {
             @Override
             public void onModify(WatchEvent<?> event, Path currentPath) {
-                String fileName = currentPath.toFile()
+                Path filePath = (Path) event.context();
+                String fileName = filePath.toFile()
                         .getName();
                 // 但公钥或者私钥发生变化时
                 if (Objects.equals(fileName, aliCdnProperties.getPub()) ||
                         Objects.equals(fileName, aliCdnProperties.getPri())) {
+                    log.info("监听到 {} 被修改，尝试执行更新任务", fileName);
                     // 主动更新一次
                     work.run();
                 }
             }
-        }, 1000L)).start();
+        }, aliCdnProperties.getFileModifyListenDelay())).start();
     }
 
 }
